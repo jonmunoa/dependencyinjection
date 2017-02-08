bc6f9d1e9c7e8e2554a5f05fa78c656df32dbfd1
jon.munoa <jmunoa91@gmail.com>
2017-02-08 10:58:10 +0100

    Creamos la interfaz vehículo y las clases que implementarán dicha interfaz.

---
+++ DependencyInjection/IVehiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public interface IVehiculo
+    {
+        string Conducir();
+    }
+
+    public class Coche : IVehiculo
+    {
+        public string Conducir()
+        {
+            return "Piloto conduciendo un BMW";
+        }
+    }
+
+    public class Moto : IVehiculo
+    {
+        public string Conducir()
+        {
+            return "Piloto conduciendo una Ducati";
+        }
+    }
+}
