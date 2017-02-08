b0fabafd39e7c51a13a39faae7801ee0aa3dc9f6
jon.munoa <jmunoa91@gmail.com>
2017-02-08 11:30:02 +0100

    Creamos la clase piloto y hacemos que implemente la interfaz IVehiculo

---
+++ DependencyInjection/Piloto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class Piloto
+    {
+        protected IVehiculo vehiculo;
+
+        public Piloto(IVehiculo _IVehiculo)
+        {
+            vehiculo = _IVehiculo;
+        }
+
+        public string Conducir()
+        {
+            return vehiculo.Conducir();
+        }
+    }
+}
